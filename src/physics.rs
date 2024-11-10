a2a7818032f17cd778063936b9994cfc03357fe7
DrxcoDev <156522801>
2024-11-10 13:01:08 +0100

    Physics settings

---
+++ src/physics.rs
@@ -0,0 +1,11 @@
+pub struct PhysicsEngine;
+
+impl PhysicsEngine {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn update(&self, delta_time: f32) {
+        // Futura implementación de físicas
+    }
+}
